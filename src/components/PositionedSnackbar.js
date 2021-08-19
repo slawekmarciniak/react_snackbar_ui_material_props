74037a78751e3b8f934537c375b4ff98f93158a2
Slawek <61154596+Ultran@users.noreply.github.com>
2021-08-19 21:56:08 +0200

    initial commit

--- src/components/PositionedSnackbar.js
+++ src/components/PositionedSnackbar.js
@@ -45,7 +45,6 @@ export default function PositionedSnackbar({ snackbarOptions }) {
         anchorOrigin={{ vertical, horizontal }}
         open={open}
         onClose={handleClose}
-        // message="I love snacks"
         key={vertical + horizontal}
         autoHideDuration={duration}
       >
